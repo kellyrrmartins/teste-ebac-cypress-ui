7f5ff638c280d09af62517f8c9420be30c76feda
Kelly Martins <kellyrdrmartins@gmail.com>
2023-01-27 19:26:11 -0300

    alteracaoes no pre cadastro e teste da pagina de produto

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -6,13 +6,17 @@ describe('Funcionalidade Pré cadastro', () => {
     cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
   })
   it('Deve completar o pré cadastro com sucesso', () => {
-    cy.get('#reg_email').type(faker.internet.email())
+    let nameFaker = faker.name.firstName()
+    let lastFaker = faker.name.lastName()
+    let emailFaker = faker.internet.email(nameFaker)
+
+    cy.get('#reg_email').type(emailFaker)
     cy.get('#reg_password').type('teste@teste.com')
     cy.get(':nth-child(4) > .button').click()
 
     cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-    cy.get('#account_first_name').type(faker.name.firstName())
-    cy.get('#account_last_name').type(faker.name.lastName())
+    cy.get('#account_first_name').type(nameFaker)
+    cy.get('#account_last_name').type(lastFaker)
     cy.get('.woocommerce-Button').click()
     cy.get('.woocommerce-message').should(
       'contain',
---
+++ cypress/e2e/produtos.cy.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade Página de produto', () => {
+  beforeEach(() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/produtos/page/2/')
+  })
+
+  it('Deve selecionar um produto da lista', () => {
+    cy.get('[class="product-block grid"]')
+      // .first()
+      // .last()
+      // .eq(3)
+      .contains('Atomic Endurance Running Tee (V-neck)')
+      .click()
+  })
+
+  it.only('Deve adicionar um produto ao carrinho', () => {
+    let quantidade = 8
+    cy.get('[class="product-block grid"]')
+      .contains('Atomic Endurance Running Tee (V-neck)')
+      .click()
+    cy.get('.button-variable-item-M').click()
+    cy.get('.button-variable-item-Yellow').click()
+    cy.get('.input-text').clear().type(quantidade)
+    cy.get('.single_add_to_cart_button').click()
+    cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+    cy.get('.woocommerce-message').should(
+      'contain',
+      `${quantidade} × “Atomic Endurance Running Tee (V-neck)” foram adicionados no seu carrinho.`
+    )
+  })
+})
